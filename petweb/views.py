bb2d7e7d3287df29878a9b81059c21375308c23d
Zak Boulaich <2879545b@student.gla.ac.uk>
2025-03-10 16:39:29 +0000

    4.1

--- petweb/views.py
+++ petweb/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    return render(request,"petweb/index.html")
 
 def about(request):
     return HttpResponse("Hello from the about")
